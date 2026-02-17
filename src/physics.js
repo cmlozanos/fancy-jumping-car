f5b525f54e9d4f05b935a7b6cb2ac763c2d11695
carlosmls-ext-inditex <carlosmls@ext.inditex.com>
2026-02-17 13:29:43 +0100

    add freeze on finish

--- src/physics.js
+++ src/physics.js
@@ -2,6 +2,16 @@ import { updateCarPhysics } from "./physics/carPhysics.js";
 import { updateWorldPhysics } from "./physics/worldPhysics.js";
 
 export function updatePhysics({ delta, state, input, track, trackOps, world }) {
+  if (state.finished) {
+    state.speed = 0;
+    state.lateralVel = 0;
+    state.jumpVel = 0;
+    return {
+      center: trackOps.getPoint(track, state.progress),
+      tangent: trackOps.getTangent(track, state.progress),
+    };
+  }
+
   const { center, tangent } = updateCarPhysics({
     delta,
     state,
